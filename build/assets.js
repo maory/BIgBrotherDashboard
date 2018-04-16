eae5a2ec600900134b92ac01caf3c34e8e0cf538
maory <mkrembo@gmail.com>
2018-04-16 14:28:16 +0300

    This is the first

---
+++ build/assets.js
@@ -0,0 +1,1 @@
+module.exports = {"main":{"js":"/assets/main.js?505647c91a53771889f0"}};
---
+++ src/components/App/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+
+  it('renders children correctly', () => {
+    const wrapper = shallow(
+      <App context={{ insertCss: () => {} }}>
+        <div className="child" />
+      </App>
+    );
+
+    expect(wrapper.contains(<div className="child" />)).to.be.true;
+  });
+
+});
